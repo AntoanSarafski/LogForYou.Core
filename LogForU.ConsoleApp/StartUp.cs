78d8a9a261511e5a16d788d377557a743330ed27
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-17 23:31:01 +0200

    Commit with tested logic.

--- LogForU.ConsoleApp/StartUp.cs
+++ LogForU.ConsoleApp/StartUp.cs
@@ -1,5 +1,6 @@
 ﻿using LogForU.Core.Appenders;
 using LogForU.Core.Appenders.Interfaces;
+using LogForU.Core.Enums;
 using LogForU.Core.Loggers;
 using LogForU.Core.Loggers.Interfaces;
 using System;
@@ -10,7 +11,7 @@ namespace LogForU.ConsoleApp
     {
         static void Main(string[] args)
         {
-            IAppender appender = new ConsoleAppender();
+            IAppender appender = new ConsoleAppender(ReportLevel.Error);
 
             ILogger logger = new Logger(appender);
 

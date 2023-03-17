ea92e57ce007c9ee40130874249bd855e9c7a2b5
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-18 00:13:28 +0200

    Created DateTime Format validator.

---
+++ LogForYou.Core/Utils/DateTimeValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace LogForU.Core.Utils
+{
+    public static class DateTimeValidator
+    {
+        public static bool ValidateDateTime(string createdTime)
+        {
+            string format = "M/dd/yyyy h:mm:ss tt";
+            if (DateTime.TryParseExact(
+                createdTime, 
+                format, 
+                CultureInfo.InvariantCulture,
+                DateTimeStyles.None, 
+                out DateTime result))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}

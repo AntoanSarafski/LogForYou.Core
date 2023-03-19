95780dba598b6b50226482793895509255cf24fd
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-19 12:28:09 +0200

    Commit with starting make  LogFile IO

---
+++ LogForYou.Core/IO/Interfaces/ILogFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogForU.Core.IO.Interfaces
+{
+    public interface ILogFile
+    {
+        string Name { get; }
+
+        string Extenxion { get; }
+
+        string Path { get; }
+
+        string Content { get; }
+
+        int Size { get; }
+
+        void WriteLine(string text);
+    }
+}
---
+++ LogForYou.Core/IO/LogFile.cs
@@ -0,0 +1,45 @@
+﻿using LogForU.Core.IO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogForU.Core.IO
+{
+    public class LogFile : ILogFile
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Extenxion
+        {
+            get;
+            set;
+        }
+
+        public string Path
+        {
+            get;
+            set;
+        }
+
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        public int Size
+        {
+            get;
+            set;
+        }
+
+        public void WriteLine(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
